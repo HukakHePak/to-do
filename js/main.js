d86b39230391f0e9b52508bd5f2d13f6f4d76905
HukakHePak <valsidalvonilb@outlook.com>
2021-12-04 20:06:21 +0400

    first workimg version

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-import { HIGH_CONTAINER } from "./view";
-import { LOW_CONTAINER } from "./view";
-import { FORMS } from "./view";
+import { FORMS } from "./view.js";
 
+export function createNewTask(form) {
+    if(!checkMaxHeight(form.parentNode)) return;
+
+    let task = FORMS.task.cloneNode(true);
+    task.children[1].textContent = form.firstElementChild.value;
+    form.firstElementChild.value = '';
+
+    task.onsubmit = () => { 
+        task.remove(); 
+        return false;
+    };
+
+    task.firstElementChild.onclick = () => { 
+        task.classList.toggle('checked');
+    };
+
+    form.after(task);
+}
+
+function checkMaxHeight(form) {
+    return form.children.length < 20;
+}
--- js/view.js
+++ js/view.js
@@ -1,19 +1,26 @@
+import { createNewTask } from "./main.js";
 
 export const HIGH_CONTAINER = document.querySelector('.container');
 
 export const FORMS = {
     name: HIGH_CONTAINER.firstElementChild,
-    add: HIGH_CONTAINER.querySelector('.add'),
+    add: HIGH_CONTAINER.children[1],
     task: HIGH_CONTAINER.lastElementChild,
 }
 HIGH_CONTAINER.lastElementChild.remove();
 
+FORMS.add.onsubmit = () => { 
+    createNewTask(FORMS.add); 
+    return false; 
+};
+
+
 export const LOW_CONTAINER = HIGH_CONTAINER.cloneNode(true);
 LOW_CONTAINER.firstElementChild.textContent = "LOW";
 
+LOW_CONTAINER.children[1].onsubmit = () => {
+    createNewTask(LOW_CONTAINER.children[1]);
+    return false;
+};
+
 HIGH_CONTAINER.after(LOW_CONTAINER);
-
-
-
-
-console.log(LOW_CONTAINER);
