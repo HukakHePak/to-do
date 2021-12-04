c570edf1973970a458fce3c9719732e9c53401ac
HukakHePak <valsidalvonilb@outlook.com>
2021-12-05 00:36:05 +0400

    remake functions to more clean

--- js/main.js
+++ js/main.js
@@ -1,38 +1,33 @@
-import { FORMS } from "./view.js";
-import { CONTAINERS } from "./view.js";
+import { TASK, CONTAINERS } from "./view.js";
 
-export function createNewTask(form) {
-    let input = form.firstElementChild;
+export function addNewTask() {
+    let input = this.firstElementChild;
     let value = input.value.trim();
-    
-    let rightSizeAndNotEmpty = form.children.length < CONTAINERS.maxCount && value.length;
 
-    if(rightSizeAndNotEmpty) {   
-        let task = FORMS.task.cloneNode(true);
+    if(value.length) {   
+        let task = TASK.cloneNode(true);
         task.children[1].textContent = value;
-    
-        task.onsubmit = () => { 
-            task.remove(); 
-            return false;
-        };
-
-        task.firstElementChild.onclick = () => { 
-            task.classList.toggle('checked');
-        };
-        form.after(task);
+        task.onsubmit = deleteTask;
+        task.onchange = changeStatus;
+        this.after(task);
     }  
     input.value = '';
     return false;
 }
 
-FORMS.add.onsubmit = () => { 
-    createNewTask(FORMS.add); 
-    return false; 
-};
-
-CONTAINERS.low.children[1].onsubmit = () => {
-    createNewTask(CONTAINERS.low.children[1]);
+function changeStatus() {
+    this.classList.toggle('checked');
     return false;
-};
+}
 
-console.log(CONTAINERS.low);
+function deleteTask() {
+    this.remove();
+    return false;
+}
+
+for(let item in CONTAINERS) {
+    let form = CONTAINERS[item].children[1];
+    form.onsubmit = addNewTask;
+}
+
+
--- js/view.js
+++ js/view.js
@@ -1,20 +1,14 @@
 const mainContainer = document.querySelector('.container');
 
+export const TASK = mainContainer.lastElementChild;
+
+TASK.remove();
+
 export const CONTAINERS = {
     high: mainContainer,
     low: mainContainer.cloneNode(true),
-    maxCount: 20
 }
 
-export const FORMS = {
-    name: CONTAINERS.high.firstElementChild,
-    add: CONTAINERS.high.children[1],
-    task: CONTAINERS.high.lastElementChild,
-}
-
-CONTAINERS.high.lastElementChild.remove();
-CONTAINERS.low.lastElementChild.remove();
 CONTAINERS.low.firstElementChild.textContent = "LOW";
-
 CONTAINERS.high.after(CONTAINERS.low);
 
